e94239f36828f2772872597b63d78a773f8daae1
Haruka Fujii <harukafujii.dev@gmail.com>
2025-09-05 10:02:34 -0700

    feat: added lab0

---
+++ Lab/Lab0/MyInformation.java
@@ -0,0 +1,14 @@
+
+public class MyInformation {
+	public static void main(String[] args) 
+	{
+
+		 System.out.println(" ****************************************************");
+		 System.out.println("*                                                    *");
+		 System.out.println("*     Name: Haruka Fujii                             *");
+		 System.out.println("*     Student Number: 100444473                      *");
+		 System.out.println("*     Courses: CPSC1150, SCIE1114, FREN1115          *");
+		 System.out.println("*                                                    *");
+		 System.out.println(" ****************************************************");	 
+	}
+}
